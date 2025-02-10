0ce44943fe7db91ea93deaad9e0da3646193170c
Matthew Peng <mapeng@ucsd.edu>
2025-02-09 21:19:29 -0800

    initial cnn test on original mnist

--- phospene_generator.py
+++ phospene_generator.py
@@ -1,8 +1,6 @@
 import io
-import logging
 import time
 
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pulse2percept as p2p
@@ -11,16 +9,18 @@ from PIL import Image
 
 def generate_percept():
     train_df = pd.read_parquet("datasets/MNIST/train.parquet")
-    logging.info(f"train_df shape = {train_df.shape}")
 
     implant = p2p.implants.PRIMA75()
-    model = p2p.models.ScoreboardModel(xrange=(-2.5, 2), yrange=(-2, 2), rho=20, xystep=0.05)
+    model = p2p.models.ScoreboardModel(xrange=(-2, 2), yrange=(-2, 2), xystep=0.15, rho=20)
     model.build()
 
     processed = []
+    labels = []
 
-    for i in range(10):
-        image = Image.open(io.BytesIO(train_df["image"].iloc[0]['bytes']))
+    for i in range(train_df.shape[0]):
+        if i % 1000 == 0: print(i)
+        image = Image.open(io.BytesIO(train_df["image"].iloc[i]['bytes']))
+        labels.append(train_df["label"].iloc[i])
         img_array = np.array(image)
         stim = p2p.stimuli.ImageStimulus(img_array)
 
@@ -31,7 +31,34 @@ def generate_percept():
         processed.append(frame)
 
     print(len(processed))
-    print((processed[0] == processed[1]).all())
+    print(len(labels))
+
+    np.savez_compressed("Out/traindata.npz", data=processed)
+    np.savez_compressed("Out/trainlabels.npz", data=labels)
+
+    test_df = pd.read_parquet("datasets/MNIST/test.parquet")
+
+    processed = []
+    labels = []
+
+    for i in range(test_df.shape[0]):
+        if i % 1000: print(i)
+        image = Image.open(io.BytesIO(test_df["image"].iloc[i]['bytes']))
+        labels.append(test_df["label"].iloc[i])
+        img_array = np.array(image)
+        stim = p2p.stimuli.ImageStimulus(img_array)
+
+        implant.stim = stim.resize(implant.shape)
+        percept = model.predict_percept(implant)
+
+        frame = percept.max(axis='frames')
+        processed.append(frame)
+
+    print(len(processed))
+    print(len(labels))
+
+    np.savez_compressed("Out/testdata.npz", data=processed)
+    np.savez_compressed("Out/testlabels.npz", data=labels)
 
 def main():
     start_time = time.time()
